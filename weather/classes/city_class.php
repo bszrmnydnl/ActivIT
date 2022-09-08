ffb117c94dea1fffd2c4c241d1f29dec91c31d29
Böszörményi Dániel <48497813+bszrmnydnl@users.noreply.github.com>
2022-09-08 10:13:30 +0200

    drupal module

---
+++ weather/classes/city_class.php
@@ -0,0 +1,23 @@
+<?php
+
+class City
+{
+    public function getTemp($latitude, $longitude){
+        $url = 'https://api.met.no/weatherapi/locationforecast/2.0/compact?lat='. $latitude .'&lon=' . $longitude;
+        $curl = curl_init($url);
+
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, [
+            'User-Agent: testing boszormenyi.daniel@outlook.com',
+            'Accept: application/json'
+        ]);
+
+        $response = curl_exec($curl);
+
+        curl_close($curl);
+
+        return json_encode(json_decode($response, true)["properties"]["timeseries"]["0"]["data"]["instant"]["details"]["air_temperature"]);
+    }
+}
